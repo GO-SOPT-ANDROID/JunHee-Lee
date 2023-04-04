83748c2e4055b352a85781cef0aafbe8e900300e
l2zh <dlwnsgml0109@naver.com>
2023-04-04 17:10:06 +0900

    로그인 뷰 디자인,구현 회원가입 뷰 디자인, 구현

--- app/src/main/java/org/android/go/sopt/HomeActivity.kt
+++ app/src/main/java/org/android/go/sopt/HomeActivity.kt
@@ -3,9 +3,9 @@ package org.android.go.sopt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
