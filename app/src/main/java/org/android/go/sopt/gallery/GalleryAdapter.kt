8e162a2290c441b61fc82569d81c589a37086245
l2zh <dlwnsgml0109@naver.com>
2023-05-11 02:32:57 +0900

    뷰페이저 구현 완료

--- app/src/main/java/org/android/go/sopt/gallery/GalleryAdapter.kt
+++ app/src/main/java/org/android/go/sopt/gallery/GalleryAdapter.kt
@@ -1,4 +1,4 @@
-package org.android.go.sopt
+package org.android.go.sopt.gallery
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -6,32 +6,36 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import org.android.go.sopt.Pager
 import org.android.go.sopt.databinding.ItemPagerBinding
 
 
-class GalleryAdapter (context: Context, _itemList: List<Int> = listOf()): ListAdapter<Pager, GalleryAdapter.GalleryViewHolder>(GalleryAdapter.diffUtil) {
-    private val inflater by lazy { LayoutInflater.from(context)}
-    private var itemList1: List<Int> = _itemList
+class GalleryAdapter(context: Context) :
+    ListAdapter<Pager, GalleryAdapter.GalleryViewHolder>(
+        diffUtil
+    ) {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): GalleryAdapter.GalleryViewHolder {
+    ): GalleryViewHolder {
         val binding = ItemPagerBinding.inflate(inflater, parent, false)
-        return GalleryAdapter.GalleryViewHolder(binding)
+        return GalleryViewHolder(binding)
     }
 
 
     override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
-        holder.bind(src = R.drawable.pic1)
+        holder.onbind(getItem(position))
     }
 
 
     class GalleryViewHolder(private val binding: ItemPagerBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(src: Int) {
-            binding.imgGallery.setImageResource(src)
+        fun onbind(src: Pager) {
+            binding.imgGallery.setImageResource(src.image_ex)
         }
     }
 
--- app/src/main/java/org/android/go/sopt/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/gallery/GalleryFragment.kt
@@ -1,10 +1,12 @@
-package org.android.go.sopt
+package org.android.go.sopt.gallery
 
-import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.android.go.sopt.Pager
+import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentGalleryBinding
 
 
@@ -42,7 +44,9 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val galleryAdapter = GalleryAdapter(requireContext())
+        binding.pagerGallery.adapter = galleryAdapter
+        galleryAdapter.submitList(itemlist)
     }
 
     override fun onDestroyView() {
